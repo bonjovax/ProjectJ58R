4f1a11fac87f25cba2e813eefe8292c28e384929
Alfel Benvic Go <dlg.alfel@gmail.com>
2014-09-24 13:46:16 +0800

    CRM
    Summary Resize due to low-res screen

--- nPOSProj/mdiSummary.Designer.cs
+++ nPOSProj/mdiSummary.Designer.cs
@@ -41,13 +41,6 @@
             this.btnEsc = new System.Windows.Forms.Button();
             this.btnF12 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblOut = new System.Windows.Forms.Label();
-            this.lblCurrent = new System.Windows.Forms.Label();
-            this.lblthirty = new System.Windows.Forms.Label();
-            this.lblsixty = new System.Windows.Forms.Label();
-            this.lblninty = new System.Windows.Forms.Label();
-            this.lblOver = new System.Windows.Forms.Label();
             this.customer = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.terms = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.duedate = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -82,10 +75,9 @@
             this.btnEsc.ForeColor = System.Drawing.Color.White;
             this.btnEsc.Image = ((System.Drawing.Image)(resources.GetObject("btnEsc.Image")));
             this.btnEsc.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnEsc.Location = new System.Drawing.Point(1272, 1);
-            this.btnEsc.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEsc.Location = new System.Drawing.Point(1124, 1);
             this.btnEsc.Name = "btnEsc";
-            this.btnEsc.Size = new System.Drawing.Size(95, 42);
+            this.btnEsc.Size = new System.Drawing.Size(94, 42);
             this.btnEsc.TabIndex = 13;
             this.btnEsc.Text = "(Esc) Exit";
             this.btnEsc.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -105,8 +97,7 @@
             this.btnF12.ForeColor = System.Drawing.Color.White;
             this.btnF12.Image = ((System.Drawing.Image)(resources.GetObject("btnF12.Image")));
             this.btnF12.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnF12.Location = new System.Drawing.Point(1115, 1);
-            this.btnF12.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnF12.Location = new System.Drawing.Point(967, 1);
             this.btnF12.Name = "btnF12";
             this.btnF12.Size = new System.Drawing.Size(157, 42);
             this.btnF12.TabIndex = 14;
@@ -135,7 +126,7 @@
             this.overninty});
             dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.ControlText;
             dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.CornflowerBlue;
             dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
@@ -143,92 +134,14 @@
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle7;
             this.dataGridView1.Enabled = false;
             this.dataGridView1.Location = new System.Drawing.Point(1, 44);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             dataGridViewCellStyle8.BackColor = System.Drawing.Color.Gainsboro;
             this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle8;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1368, 438);
+            this.dataGridView1.Size = new System.Drawing.Size(1218, 439);
             this.dataGridView1.TabIndex = 20;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(258, 486);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(215, 17);
-            this.label1.TabIndex = 21;
-            this.label1.Text = "Total Accounts Receivables Aging";
-            // 
-            // lblOut
-            // 
-            this.lblOut.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOut.ForeColor = System.Drawing.Color.White;
-            this.lblOut.Location = new System.Drawing.Point(481, 488);
-            this.lblOut.Name = "lblOut";
-            this.lblOut.Size = new System.Drawing.Size(140, 17);
-            this.lblOut.TabIndex = 22;
-            this.lblOut.Text = "0.00";
-            this.lblOut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblCurrent
-            // 
-            this.lblCurrent.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCurrent.ForeColor = System.Drawing.Color.White;
-            this.lblCurrent.Location = new System.Drawing.Point(630, 488);
-            this.lblCurrent.Name = "lblCurrent";
-            this.lblCurrent.Size = new System.Drawing.Size(140, 17);
-            this.lblCurrent.TabIndex = 23;
-            this.lblCurrent.Text = "0.00";
-            this.lblCurrent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblthirty
-            // 
-            this.lblthirty.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblthirty.ForeColor = System.Drawing.Color.White;
-            this.lblthirty.Location = new System.Drawing.Point(781, 488);
-            this.lblthirty.Name = "lblthirty";
-            this.lblthirty.Size = new System.Drawing.Size(140, 17);
-            this.lblthirty.TabIndex = 24;
-            this.lblthirty.Text = "0.00";
-            this.lblthirty.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblsixty
-            // 
-            this.lblsixty.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblsixty.ForeColor = System.Drawing.Color.White;
-            this.lblsixty.Location = new System.Drawing.Point(927, 488);
-            this.lblsixty.Name = "lblsixty";
-            this.lblsixty.Size = new System.Drawing.Size(140, 17);
-            this.lblsixty.TabIndex = 25;
-            this.lblsixty.Text = "0.00";
-            this.lblsixty.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblninty
-            // 
-            this.lblninty.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblninty.ForeColor = System.Drawing.Color.White;
-            this.lblninty.Location = new System.Drawing.Point(1085, 488);
-            this.lblninty.Name = "lblninty";
-            this.lblninty.Size = new System.Drawing.Size(140, 17);
-            this.lblninty.TabIndex = 26;
-            this.lblninty.Text = "0.00";
-            this.lblninty.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblOver
-            // 
-            this.lblOver.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOver.ForeColor = System.Drawing.Color.White;
-            this.lblOver.Location = new System.Drawing.Point(1228, 488);
-            this.lblOver.Name = "lblOver";
-            this.lblOver.Size = new System.Drawing.Size(140, 17);
-            this.lblOver.TabIndex = 27;
-            this.lblOver.Text = "0.00";
-            this.lblOver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // customer
             // 
@@ -265,7 +178,7 @@
             this.current.HeaderText = "Current";
             this.current.Name = "current";
             this.current.ReadOnly = true;
-            this.current.Width = 150;
+            this.current.Width = 120;
             // 
             // thirties
             // 
@@ -274,7 +187,7 @@
             this.thirties.HeaderText = "1-30";
             this.thirties.Name = "thirties";
             this.thirties.ReadOnly = true;
-            this.thirties.Width = 150;
+            this.thirties.Width = 120;
             // 
             // sixties
             // 
@@ -283,7 +196,7 @@
             this.sixties.HeaderText = "31-60";
             this.sixties.Name = "sixties";
             this.sixties.ReadOnly = true;
-            this.sixties.Width = 150;
+            this.sixties.Width = 120;
             // 
             // nineties
             // 
@@ -292,7 +205,7 @@
             this.nineties.HeaderText = "61-90";
             this.nineties.Name = "nineties";
             this.nineties.ReadOnly = true;
-            this.nineties.Width = 150;
+            this.nineties.Width = 120;
             // 
             // overninty
             // 
@@ -301,30 +214,22 @@
             this.overninty.HeaderText = "91+";
             this.overninty.Name = "overninty";
             this.overninty.ReadOnly = true;
-            this.overninty.Width = 150;
+            this.overninty.Width = 120;
             // 
             // mdiSummary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SeaGreen;
-            this.ClientSize = new System.Drawing.Size(1370, 509);
+            this.ClientSize = new System.Drawing.Size(1220, 509);
             this.ControlBox = false;
-            this.Controls.Add(this.lblOver);
-            this.Controls.Add(this.lblninty);
-            this.Controls.Add(this.lblsixty);
-            this.Controls.Add(this.lblthirty);
-            this.Controls.Add(this.lblCurrent);
-            this.Controls.Add(this.lblOut);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnF12);
             this.Controls.Add(this.btnEsc);
             this.Controls.Add(this.dtSelect);
             this.Cursor = System.Windows.Forms.Cursors.Default;
-            this.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "mdiSummary";
@@ -334,7 +239,6 @@
             this.Text = "Summary";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -344,13 +248,6 @@
         private System.Windows.Forms.Button btnEsc;
         private System.Windows.Forms.Button btnF12;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblOut;
-        private System.Windows.Forms.Label lblCurrent;
-        private System.Windows.Forms.Label lblthirty;
-        private System.Windows.Forms.Label lblsixty;
-        private System.Windows.Forms.Label lblninty;
-        private System.Windows.Forms.Label lblOver;
         private System.Windows.Forms.DataGridViewTextBoxColumn customer;
         private System.Windows.Forms.DataGridViewTextBoxColumn terms;
         private System.Windows.Forms.DataGridViewTextBoxColumn duedate;
